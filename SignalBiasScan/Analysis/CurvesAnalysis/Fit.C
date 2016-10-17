e35359e7b20a591735ff4602becad35796fba98c
jlagram <jlagram@cern.ch>
2016-10-17 12:27:17 +0200

    add 2 lines fit of curves

--- SignalBiasScan/Analysis/CurvesAnalysis/Fit.C
+++ SignalBiasScan/Analysis/CurvesAnalysis/Fit.C
@@ -34,6 +34,42 @@ Double_t fitpol(Double_t *x, Double_t *par){
   return value;
 }
 
+double getCrossingPoint ( TF1* function1, TF1* function2)
+{
+	double x_crossingpoint;
+	double oao_function1, cd_function1, oao_function2, cd_function2; 
+	oao_function1 = function1->GetParameter(0); // ordonnée à l'origine
+	cd_function1 = function1->GetParameter(1); // coefficient directeur
+	oao_function2 = function2->GetParameter(0);// ordonnée à l'origine
+	cd_function2 = function2->GetParameter(1);// coefficient directeur
+
+	// at the crossing point, we have cd_function1*x_crossingpoint + oao_function1 = cd_function2*x_crossingpoint + oao_function2
+	// it is the same as having x_crossingpoint = (oao_function2 - oao_function1)/(cd_function1 - cd_function2 )
+	x_crossingpoint = (oao_function2 - oao_function1)/(cd_function1 - cd_function2 );
+
+return x_crossingpoint;
+}
+
+Double_t GetKinkByFit(Double_t *x, Double_t *par)
+{
+  Double_t value, value_th, value_0;
+  if (x[0]<par[2])
+  {
+        value = par[0] + par[1]*x[0] + par[3]*x[0]*x[0]; // pol2
+        //value = par[0] + par[1]*x[0];
+  }
+  else
+  {
+        //value_th = par[0] + par[1]*par[2]
+        value_th = par[0] + par[1]*par[2] + par[3]*par[2]*par[2] ;
+        value_0 = value_th - par[4]*par[2];
+        value = value_0 + par[4]*x[0]; // pol1
+  }
+
+  return value;
+}
+
+
 double FitCurve(TGraphErrors* g, int debug=0, bool filter_twice=false)
 {
 
@@ -71,6 +107,7 @@ double FitCurve(TGraphErrors* g, int debug=0, bool filter_twice=false)
     if(y>ymax) {ymax=y; vdep1=xlow;}
     if(y<ymin) ymin=y;
   }
+  cout<<"Vdep by highest point = "<<vdep1<<endl;
 
 
   // Compute a voltage threshold for Kink finding algo
@@ -93,6 +130,9 @@ double FitCurve(TGraphErrors* g, int debug=0, bool filter_twice=false)
   cout<<"x thresh : "<<xthresh<<endl;
   
   
+  // Fitting the curve by line from the right until it is no more compatible with a line
+  // Stopping point can be considerered as the saturation point
+  //------------------------------------------------------------------------------------
   //g=gmedian;
   int npt = g->GetN()-5;
   xlow=350;
@@ -104,7 +144,7 @@ double FitCurve(TGraphErrors* g, int debug=0, bool filter_twice=false)
     f3->SetRange(xlow, 350);
     status = g->Fit("fp1", "rqn");
     chi2 = f3->GetChisquare()/f3->GetNDF();
-    if(debug>=1) cout<<"xlow "<<xlow<<" chi2 "<<chi2<<endl;
+    if(debug>=1) cout<<" xlow "<<xlow<<" chi2 "<<chi2<<endl;
 	npt--;
   }
   g->GetPoint(npt+2, xlow, y);
@@ -117,9 +157,76 @@ double FitCurve(TGraphErrors* g, int debug=0, bool filter_twice=false)
   //else if(fabs(vdep-vdep1)>40) vdep = vdep1;
   vdep=vdep1;
 
-  cout<<" Vdepl = "<<vdep<<endl;
+  cout<<"Vdep by 1 line = "<<xlow<<endl;
   
   
+  
+  // Fitting the curves with 2 lines (done for cluster width curves in TOB)
+  // Crossing point is the saturation point
+  //-----------------------------------------
+  
+  TF1* f3_high = new TF1("fphigh", "pol1", 20, 360);
+  f3_high->SetLineColor(4);
+  TF1* f3_low = new TF1("fplow", "pol1", 20, 360);
+  f3_low->SetLineColor(kGreen);
+  
+  //g=gmedian;
+ 
+  int npt_high = g->GetN()-3; //to take at least the 4 last points into account for the fit with the blue line
+  int npt_low_init  = 2; //don't take the 4 first points into account for the fit with the green line
+  int npt_low  = 7; //to start the fit with the 4th, 5th, 6th and 7th point (arbitrary at least 4points)
+  double vdep_high = 350;
+  int tmp_marker = 0; //Marker not to take into account the points which have a too big first chi2
+  double vdep_low  = 0; //No matter the value 
+  double vdep_low_init = 0; //No matter the value
+  g->GetPoint(npt_low_init, vdep_low_init, y);//To set the minimum of the range to the V of the 4th point 
+  //cout << "Vdep_low_init= " << vdep_low_init <<  endl;
+  xlow = 350;
+  double chi2_high=0, chi2_low=0;
+  
+  //Process the fit from the right
+  // chi2_high < 0.012 in TOB CW
+  while(chi2_high < 2. && vdep_high > 100 && npt_high >= 0)
+  {
+    g->GetPoint(npt_high, vdep_high, y);
+	//cout<<"fit right: "<<npt_high<<"  vdep "<<vdep_high<<endl;
+    f3_high->SetRange(vdep_high, 350);
+    status = g->Fit("fphigh", "rqn");
+    chi2_high = f3_high->GetChisquare()/f3_high->GetNDF();
+    if(chi2_high > 50 && npt_high == g->GetN() - 4) tmp_marker = 100; // Not to deal with the bad modules
+	//cout<<"fit right: "<<npt_high<<"  chi2 "<<chi2_high<<endl;
+	npt_high--;
+  }
+  if(npt_high+2<g->GetN()-3)
+  {
+    g->GetPoint(npt_high+2, vdep_high, y);
+    f3_high->SetRange(vdep_high, 350);
+    g->Fit("fphigh", "rqn");
+  }
+
+  status = 0;
+  
+  //Process the fit from the left
+  while(chi2_low < 0.02 && vdep_low < 350 && npt_low < g->GetN() - 4)
+  {
+    g->GetPoint(npt_low, vdep_low, y);
+    f3_low->SetRange(vdep_low_init, vdep_low);
+    status = g->Fit("fplow", "rqn");
+    chi2_low = f3_low->GetChisquare()/f3_low->GetNDF();
+    if(chi2_low > 50 && npt_low == 7) tmp_marker = 100; // Not to deal with the bad modules
+	npt_low++;
+  }
+  g->GetPoint(npt_low-2, vdep_low, y);
+  f3_low->SetRange(vdep_low_init, vdep_low);
+  g->Fit("fplow", "rqn");
+
+  double vdep_crossingpoint = getCrossingPoint(f3_low , f3_high);
+  cout << "Vdep by 2 lines = " << vdep_crossingpoint << endl;
+  
+  
+  
+  // Fitting the curve with a sigmoid or a composite function
+  //----------------------------------------------------------
   
   TF1* f1 = new TF1("fitsigmo", fitsigmo, 50, 350, 5);
   f1->SetParameter(0, 3.);
@@ -175,42 +282,49 @@ double FitCurve(TGraphErrors* g, int debug=0, bool filter_twice=false)
 */
 
 
+  // Getting the kink of the curve after smoothing
+  // In using the curvature
+  //-----------------------------------------------
+  TGraphErrors* gsmooth = gmedian;
+  if(SGSmooth) gsmooth = SavitzkyGolaySmoother(gmedian, 5, 0);
+  else{
+    gsmooth = HanningFilter(gmedian);
+    // To check : 2nd hanningFilter better with 3 or 5 points ?
+    if(filter_twice) gsmooth = HanningFilter(gsmooth); // For TOB, some scans for TID
+  }
 
-      TGraphErrors* gsmooth = gmedian;
-      if(SGSmooth) gsmooth = SavitzkyGolaySmoother(gmedian, 5, 0);
-      else{
-        gsmooth = HanningFilter(gmedian);
-        // To check : 2nd hanningFilter better with 3 or 5 points ?
-        if(filter_twice) gsmooth = HanningFilter(gsmooth); // For TOB, some scans for TID
-      }
+  TGraphErrors* gscurv;
+  if(SGSmooth) gscurv = GetSavitzkyGolayCurvatureGraph( gmedian );
+  else gscurv = GetCurvatureGraph( gsmooth );
+  gscurv->SetMarkerStyle(20);
+  TGraph* g3pts = new TGraph();
+  float xopt = GetOptimalMinNPts(gscurv, g3pts, xthresh);
 
-      TGraphErrors* gscurv;
-      if(SGSmooth) gscurv = GetSavitzkyGolayCurvatureGraph( gmedian );
-      else gscurv = GetCurvatureGraph( gsmooth );
-      gscurv->SetMarkerStyle(20);
-      TGraph* g3pts = new TGraph();
-      float xopt = GetOptimalMinNPts(gscurv, g3pts, xthresh);
+  vdep = xopt;
 
-      vdep = xopt;
-
-      cout<<" Kink = "<<xopt<<endl;
+  cout<<"Vdep by kink = "<<xopt<<endl;
 
 
+  // Drawing the curves and fits
   TCanvas *c1;
   TCanvas *c2;
   if(debug>=1)
   {
     c1 = new TCanvas();
     g->Draw("AP");
+    g->GetXaxis()->SetTitle("V_{bias} [V]");
+    //g->GetYaxis()->SetTitle("ClusterWidth [#strips]");
     //f2->Draw("same");
     //f3->Draw("same");
 	gsmooth->SetMarkerColor(17);
 	gsmooth->Draw("P");
-gmedian->SetMarkerColor(15);
-//gmedian->Draw("P");	
+	gmedian->SetMarkerColor(15);
+	//gmedian->Draw("P");	
 	TLine *l = new TLine(vdep, ymin, vdep, ymax+0.1);
 	l->SetLineStyle(3);
 	l->Draw();
+	
+	// draw low threshold
 	TLine *lthresh = new TLine(xthresh, ymin, xthresh, ymax+0.1);
 	lthresh->SetLineStyle(2);
 	//lthresh->Draw();
@@ -223,6 +337,13 @@ gmedian->SetMarkerColor(15);
 	gthresh->SetFillStyle(3354);
 	//gthresh->Draw("f");
  
+    // draw 2 line fit and crossing point position
+	f3_high->Draw("same");  // blue line 
+    f3_low->Draw("same");  // green line 
+	TLine *lcross = new TLine(vdep_crossingpoint, ymin, vdep_crossingpoint, ymax+0.1);
+	lcross->SetLineStyle(2);
+	lcross->Draw(); //dashed vdep line 
+
     c1->Modified();
     c1->Update();
 
@@ -232,6 +353,8 @@ gmedian->SetMarkerColor(15);
 
     c2 = new TCanvas;
     gscurv->Draw("AP");
+    gscurv->GetXaxis()->SetTitle("V_{bias} [V]");
+    gscurv->GetYaxis()->SetTitle("Curvature");
     g3pts->Draw("P");
     g3pts->Fit("pol2", "q");
     c2->Modified();
@@ -262,6 +385,7 @@ void FitOneCurve(string dirname, string subdet, string run, ULong64_t modid, str
 
   // Correct for voltage drop due to leakage current
   string corr_name="_"+subdet+run;
+  if(subdet == "TOB") modid /= 10;
   int corrected = CorrectGraphForLeakageCurrent(g, modid, corr_name);
   if(corrected) g->SetMarkerColor(13);
 
@@ -341,6 +465,23 @@ void FitTIDCurves(string dirname, string run, string type)
   FitOneCurve(dirname, "TID", run, 402677025, type, 1);
   FitOneCurve(dirname, "TID", run, 402677041, type, 1);
   
+}
+
+void FitTOBSmallScan(string dirname, string run, string type)
+{
+	FitOneCurve(dirname, "TOB", run, 4362815081, type, 1);
+	FitOneCurve(dirname, "TOB", run, 4362815082, type, 1);
+	FitOneCurve(dirname, "TOB", run, 4362815121, type, 1);
+	FitOneCurve(dirname, "TOB", run, 4362815122, type, 1);
+	FitOneCurve(dirname, "TOB", run, 4362815161, type, 1);
+	FitOneCurve(dirname, "TOB", run, 4362815162, type, 1);
+	FitOneCurve(dirname, "TOB", run, 4362815201, type, 1);
+	FitOneCurve(dirname, "TOB", run, 4362815202, type, 1);
+	FitOneCurve(dirname, "TOB", run, 4362815241, type, 1);
+	FitOneCurve(dirname, "TOB", run, 4362815242, type, 1);
+	FitOneCurve(dirname, "TOB", run, 4362815281, type, 1);
+	FitOneCurve(dirname, "TOB", run, 4362815282, type, 1);
+
 }
 
 void Fit()
