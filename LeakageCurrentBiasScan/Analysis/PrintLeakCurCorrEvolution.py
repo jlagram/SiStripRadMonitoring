a51c319e5c63558d597e80318d80b76db88f6105
jlagram <jlagram@cern.ch>
2020-12-14 19:01:33 +0100

    added noise runs

--- LeakageCurrentBiasScan/Analysis/PrintLeakCurCorrEvolution.py
+++ LeakageCurrentBiasScan/Analysis/PrintLeakCurCorrEvolution.py
@@ -24,22 +24,32 @@ def fill_runs_lumi_Run2(filename, lumi_dict):
 	
 
 # return a dictionnary of tree files for the different runs
-def list_tree_files(files_regexp):
+def list_tree_files(files_regexp, useNoiseScan=False, useNoiseScanOnly=False):
+  isNoiseScan = False
   files={}
   command='ls '+files_regexp
   p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
   output, err = p.communicate()
+  noiserun_list = []
   for line in output.splitlines(False):
     #print line
     words = line.split(" ")
     if len(words)>0 :
       #print words[0]
 	  file=words[0]
-	  run=file.split('_')[3].replace('run', '').replace('.root', '')
+	  if len(file.split('_'))>3 :
+	     if file.split('_')[2] == 'noise' : # noise scan
+	        isNoiseScan = True
+	        run=file.split('_')[4].replace('run', '').replace('.root', '')
+	        if useNoiseScan: noiserun_list.append(run)
+	     else :
+		    run=file.split('_')[3].replace('run', '').replace('.root', '')
 	  #print run
-	  if run not in files:
-	    files[run]=[]
-	  files[run].append(file)
+	  if (not isNoiseScan and not useNoiseScanOnly) or (useNoiseScan and isNoiseScan):
+	    if run not in files:
+	      files[run]=[]
+	    files[run].append(file)
+
   return files
 
 
@@ -69,10 +79,15 @@ def get_mean_temp(filename, detid):
   
   
 
-def print_leakcurcorr_evolution(files, detid=369125870, temperature=False):
+def print_leakcurcorr_evolution(files, detid=369125870, temperature=False, noiseScans=[]):
 
+  # points from signal bias scans
   g = TGraph()
   ipt=0
+  # points from noise bias scans
+  gn = TGraph()
+  iptn=0
+  # Loop on runs
   for run in sorted(files.keys()):
 	if temperature==True: corr = get_mean_temp(files[run][0], detid)
 	else: corr = get_leak_corr_at_300V(files[run][0], detid)
@@ -80,6 +95,9 @@ def print_leakcurcorr_evolution(files, detid=369125870, temperature=False):
 	  #print run, files[run][0], RUN_LUMI[run],corr
 	  g.SetPoint(ipt, RUN_LUMI[run], corr)
 	  ipt+=1
+	if run in RUN_LUMI and corr>=0 and run in noiseScans:
+	  gn.SetPoint(iptn, RUN_LUMI[run], corr)
+	  iptn+=1
 	  
   c = TCanvas()
   g.SetMarkerStyle(20)
@@ -89,6 +107,10 @@ def print_leakcurcorr_evolution(files, detid=369125870, temperature=False):
   else: h.GetYaxis().SetTitle('corr. at 300V (V)')
   g.Draw('AP')
   #g.Print('all')
+  
+  gn.SetMarkerStyle(20)
+  gn.SetMarkerColor(2)
+  gn.Draw('P')
   
   lumi_Run1 = 29.46
   l1 = TLine(lumi_Run1, h.GetMinimum(), lumi_Run1, h.GetMaximum())
@@ -126,15 +148,19 @@ RUN_LUMI={}
 fill_runs_lumi('runs_lumi_Run1.txt', RUN_LUMI)
 fill_runs_lumi_Run2('runs_lumi_Run2.txt', RUN_LUMI)
 print RUN_LUMI
-files = list_tree_files('~/work/public/SiStripRadMonitoring/LeakageCurrentCorrections/Corrections/wDCUcur/LeakCurCorr_TIB_*.root')
-files_temperature = list_tree_files('~/work/public/SiStripRadMonitoring/LeakageCurrentCorrections/Data/DCU/DCU_I_*.root')
+files = list_tree_files('~/work/public/SiStripRadMonitoring/LeakageCurrentCorrections/Corrections/wDCUcur/LeakCurCorr_TIB_*.root', True)
+files_noise = list_tree_files('~/work/public/SiStripRadMonitoring/LeakageCurrentCorrections/Corrections/wDCUcur/LeakCurCorr_TIB_*.root', True, True)
+files_temperature = list_tree_files('~/work/public/SiStripRadMonitoring/LeakageCurrentCorrections/Data/DCU/DCU_I_*.root', True)
 small_scan_detids=[369121381, 369121382, 369121385, 369121386, 369121389, 369121390, 
 369125861, 369125862, 369125865, 369125866 , 369125869 , 369125870 ]
+files_temperature_noise = list_tree_files('~/work/public/SiStripRadMonitoring/LeakageCurrentCorrections/Data/DCU/DCU_I_*.root', True, True)
 
-for mod in small_scan_detids:
-  #print_leakcurcorr_evolution(files, mod)
-  print_leakcurcorr_evolution(files_temperature, mod, True)
-#print_leakcurcorr_evolution(files, 369125870)
-#print_leakcurcorr_evolution(files_temperature, 369125870, True)
+print files_temperature_noise
+
+#for mod in small_scan_detids:
+#  print_leakcurcorr_evolution(files, mod)
+  #print_leakcurcorr_evolution(files_temperature, mod, True)
+print_leakcurcorr_evolution(files, 369121381, False, files_noise.keys())
+print_leakcurcorr_evolution(files_temperature, 369125870, True, files_noise.keys())
 
 #files = list_tree_files('~/work/public/SiStripRadMonitoring/LeakageCurrentCorrections/Data/DCU/LeakCurCorr_TIB_*.root')
