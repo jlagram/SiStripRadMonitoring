204de6593438e2b34bc5dfb9d7daeeb129b43acb
Paul Vaucelle <paul.vaucelle@etu.unistra.fr>
2025-05-13 10:50:13 +0200

    Scan May 2025

---
+++ SignalBiasScan/crab/crab3_20250509_run391825.py
@@ -0,0 +1,29 @@
+from CRABClient.UserUtilities import config 
+# ,getUsernameFromSiteDB
+config = config()
+
+config.General.requestName = 'HVscan_20250509_run391825'
+config.General.workArea = 'crab_projects'
+config.General.transferOutputs = True
+config.General.transferLogs = False
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'signalbiasscan_20250509_run391825_cfg.py'
+
+config.Data.inputDataset = '/ZeroBias/Run2025B-SiStripCalMinBias-PromptReco-v1/ALCARECO'
+# config.Data.inputBlocks = ['/ZeroBias/Run2024F-SiStripCalMinBias-PromptReco-v1/ALCARECO#9917ddf9-bdc5-4409-882d-3e3c582975c0']
+config.Data.inputDBS = 'global'
+config.Data.splitting = 'LumiBased'
+config.Data.unitsPerJob = 10 #Change to optimize file size
+config.Data.lumiMask = 'DecoHVScan_20250509_run391825_JSON.txt'
+config.Data.runRange = '391825' 
+config.Data.outLFNDirBase = '/store/group/dpg_tracker_strip/comm_tracker/Strip/RadMonitoring/SignalBiasScan/ClustersTrees/DecoSmallHVscan_20241012_run386863_v1'
+config.Data.publication = False
+#config.Data.outputDatasetTag = ''
+
+
+#Else, runs where the data are located (may be busy)
+#config.Data.ignoreLocality = True
+
+# config.Site.storageSite = 'T2_FR_IPHC'
+config.Site.storageSite = 'T2_CH_CERN'
---
+++ SignalBiasScan/crab/signalbiasscan_20250509_run391825_cfg.py
@@ -0,0 +1,55 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("Tree")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+
+ # Conditions (Global Tag is used here):
+process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag.globaltag = '150X_dataRun3_HLT_v1'
+
+process.load('Configuration.StandardSequences.Services_cff')
+
+#Geometry and field
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load("Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff")
+
+process.load('RecoVertex.BeamSpotProducer.BeamSpot_cff')
+process.load("RecoTracker.TrackProducer.TrackRefitters_cff")
+process.TrackRefitter.src = 'ALCARECOSiStripCalMinBias'
+
+
+# reduce verbosity
+process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(1000)
+
+# process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
+
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring('file:/store/data/Run2025B/ZeroBias/ALCARECO/SiStripCalMinBias-PromptReco-v1/000/391/825/00000/11966670-8713-43ca-9016-e44ffe33b4bb.root',
+))
+
+
+#name of the output file containing the tree
+process.TFileService = cms.Service("TFileService", fileName = cms.string("clustersTree.root") )
+
+process.TrackRefitter.NavigationSchool = cms.string("")
+
+process.demo = cms.EDAnalyzer('SignalBiasScan',
+    trackLabel       = cms.InputTag('ALCARECOSiStripCalMinBias'), #generalTracks
+    tkTraj           = cms.InputTag('TrackRefitter'),
+    labelTrajToTrack = cms.InputTag('TrackRefitter'),
+#   siStripClusters  = cms.InputTag('TrackRefitter'),
+    primaryVertexColl= cms.InputTag('offlinePrimaryVertices'),
+    fullHitInfo_TIB       = cms.bool(False),
+    fullHitInfo_TOB       = cms.bool(False),
+    fullHitInfo_TID       = cms.bool(False),
+    fullHitInfo_TEC       = cms.bool(False),
+    isSmallBiasScan       = cms.untracked.bool(False)
+)
+##Most common set of value for the booleans, Everything is true when smallscan, Evertything is false when Full scan
+
+process.p = cms.Path(process.MeasurementTrackerEvent*process.offlineBeamSpot*process.TrackRefitter*process.demo)
+
+
